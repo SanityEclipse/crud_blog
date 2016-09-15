944644cf19fea1b31b21f530740d39832bb1e1c0
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-09-14 23:31:26 -0600

    Initial Commit

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/crud_blog'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
